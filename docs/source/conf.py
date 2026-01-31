21cea63f557462ad8a202e9409d5a15ee6f8675e
Roman Leonov <jam_roma@yahoo.com>
2026-02-01 00:31:56 +0100

    update(docs): Added requirements and change thema
    - Fixed the push on master

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 #html_static_path = ['_static']
 html_static_path = [] # Revert static, when static assets will appear
